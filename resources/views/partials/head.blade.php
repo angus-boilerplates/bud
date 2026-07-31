b5e906195be419fde2bc82730ccbf1de432819e6
Angus <angus.goody@gmail.com>
2026-07-31 17:11:29 +0100

    moved

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -12,8 +12,7 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-@fluxAppearance
+
 
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -38,3 +37,6 @@
 
 <link rel="canonical" href="{{ request()->fullUrl() }}">
 
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+@stack('head')
+@fluxAppearance
