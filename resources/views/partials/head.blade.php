d86f27ad9d07c2e4107b6e263f0bcf29b03eb683
Angus <angus.goody@gmail.com>
2026-07-31 17:08:29 +0100

    removed duplicate tags

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -38,8 +38,3 @@
 
 <link rel="canonical" href="{{ request()->fullUrl() }}">
 
-<link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet"/>
-
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-@fluxAppearance
